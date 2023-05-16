e5716ec315fe41cc2598c685ae796c43621cb487
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-16 12:53:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ st.map(df)
 # x = st.slider('Select a value')
 # st.write(x, ' squared is ', x * x)
 
+rand = np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins = 15)
+st.pyplot(fig)
