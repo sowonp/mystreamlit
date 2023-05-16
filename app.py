7373933b90cf8b5c5ed98fe05caa54636624d78f
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-16 12:57:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt # !pip install matplotlib
 
 st.title("map")
-df = pd.DataFrame(np.random.randn(500, 2) / [50, 50] + [37.36, -122.4], colums=['lat', 'long'])
+df = pd.DataFrame(np.random.randn(500, 2) / [50, 50] + [37.36, -122.4], columns=['lat', 'lon'])
 st.map(df)
 # x = st.slider('Select a value')
 # st.write(x, ' squared is ', x * x)
