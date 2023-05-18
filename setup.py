06c5a7d4124f529087adebaaa3caab1fed3dd706
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-18 12:59:03 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from steuptools import setup
+
+setup(
+  name='logic',
+  version='0.0.1',
+  description='logic pip install bard',
+  url='https://github.com/sowonp/mystreamlit',
+  author='sowon',
+  author_email='@',
+  license='sowon',
+  package=['logic'],
+  zip_safe=False,
+  install_requires=['bard']
+)
