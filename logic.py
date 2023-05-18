ce3112c7ceeac567e56250d0a1be90f6d843da22
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-18 12:22:19 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-deduction = {'bp':'0', 'spd':'0', 'resultd':'0'}
-induction = {'indt':'0', 'spi':'0', 'resulti':'0'}
+
+deduction = {'bp':'0', 'spd':'0', 'rd':'0'}
+induction = {'indt':'0', 'spi':'0', 'ri':'0'}
 true = True
 
 tab1, tab2= st.tabs(['연역법' , '귀납법']) # 탭 이름 1: 연역법, 2: 귀납법
@@ -11,10 +12,10 @@ with tab1: #tab 1 표시될 내용
     st.write("대전제 입력: ")
     bp = st.text_input("대전제 입력: ", deduction['bp'])
     spd = st.text_input("소전제 입력: ", deduction['spd'])
-    st.write("결론: ", deduction['resultd'])
+    st.write("결론: ", deduction['rd'])
     
 with tab2: #tab 2 표시될 내용
     st.write("귀납법")
     indf = st.text_input("개별적 사실 입력: ", induction['indf'])
     spi = st.text_input("소전제 입력: ", induction['spi'])
-    st.write("결론: ", induction['resulti'])
+    st.write("결론: ", induction['ri'])
