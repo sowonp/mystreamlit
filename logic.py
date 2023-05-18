f829ebdc2ae7a43dc71064e9982970d6ef3e40e9
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-18 12:32:16 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,21 +1,19 @@
 import streamlit as st
 
 
-deduction = {'bp':'0', 'spd':'0', 'rd':'0'}
-induction = {'indt':'0', 'spi':'0', 'ri':'0'}
-true = True
+ded = {"bp":"대전제", "spd":"연역법 소전제", "rd":"연역법 결과"}
+ind = {"indf":"개별적 사실", "spi":"귀납법 소전제", "ri":"귀납법 결과"}
 
-tab1, tab2= st.tabs(['연역법' , '귀납법']) # 탭 이름 1: 연역법, 2: 귀납법
+tab1, tab2= st.tabs(['연역법' , '귀납법'])
 
-with tab1: #tab 1 표시될 내용
+with tab1:
     st.write("연역법")
-    st.write("대전제 입력: ")
-    bp = st.text_input("대전제 입력: ", deduction['bp'])
-    spd = st.text_input("소전제 입력: ", deduction['spd'])
-    st.write("결론: ", deduction['rd'])
+    bp = st.text_input("대전제 입력: ", ded["bp"])
+    spd = st.text_input("소전제 입력: ", ded["spd"])
+    st.write("결론: ", ded["rd"])
     
-with tab2: #tab 2 표시될 내용
+with tab2:
     st.write("귀납법")
-    indf = st.text_input("개별적 사실 입력: ", induction['indf'])
-    spi = st.text_input("소전제 입력: ", induction['spi'])
-    st.write("결론: ", induction['ri'])
+    indf = st.text_input("개별적 사실 입력: ", ind["indf"])
+    spi = st.text_input("소전제 입력: ", ind["spi"])
+    st.write("결론: ", ind["ri"])
