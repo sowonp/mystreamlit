0cf5326f0422718e654437351a7adae4ea9fc8b0
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-20 14:39:28 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,60 +1,24 @@
-import openai
 import streamlit as st
+import openai
 
-openai.api_key = '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'
+openai.api_key = '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'
 
-messages = []
-content = ""
-chat_response = ""
+st.title("ChatGPT with Streamlit")
 
-class MultiApp:
-    def __init__(self):
-        self.apps = []
+# 사용자 입력 받기
+user_input = st.text_input("사용자 입력")
 
-    def add_app(self, title, func):
-
-        self.apps.append({
-            "title": title,
-            "function": func
-        })
-
-    def run(self):
-
-        st.sidebar.title("추론하는 프로그램")
-        st.sidebar.subheader("어떤법으로 추론하시겠습니까?")
-               
-        app = st.sidebar.radio(
-            '',
-            self.apps,
-            format_func=lambda app: app['title'])
-    
-        app['function']()
-def g():
-    st.title("귀납법 프로그램 실행합니다.")
-    Ginfer1 = st.text_input("개별적 사실", value="여러문장 입력시 ,로 구분해서 쓰시오")
-    Ginfer2 = st.text_input("소전체")
-    if st.button("입력"):
-        content = f"귀납법으로 추론, 개별적 사실은 {Ginfer1}, 소전제는 {Ginfer2}, 결론만 출력"
-        return content
-
-def y():
-    st.title("연역법 프로그램 실행합니다.")
-    Yinfer1 = st.text_input("대전체")
-    Yinfer2 = st.text_input("소전체")
-    if st.button("입력"):
-        content = f"연역법으로 추론, 대전제는 {Yinfer1}, 소전제는 {Yinfer2}, 결론만 출력"
-        return content
-
-infer = MultiApp()
-infer.add_app("귀납법 입력", g)
-infer.add_app("연역법 입력", y)
-infer.run()
-
-if content:
+if user_input:
+    # ChatGPT에 입력 전달 및 응답 받기
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": content}]
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": user_input}
+        ]
     )
+
+    # 응답 결과 표시
     chat_response = completion.choices[0].message.content
-    st.write(chat_response)
-    messages = [{"role": "assistant", "content": chat_response}]
+    st.text("ChatGPT 응답")
+    st.text(chat_response)
