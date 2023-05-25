108c1b3e1dd1a43ec14321736f05164908a6d2c2
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-25 12:48:56 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -11,11 +11,11 @@ st.title("수리논리학 계산기")
 
 n = 0
 n = st.text_input("개별적 사실을 몇 개 입력하시겠습니까? (최대 5개)", key="infer")
+if n == "" or n == "0":
+    n = 0
+else:
+    n = int(n.split()[0])
 if st.button("입력", key="number1"):
-    if n == "" or n == "0":
-        n = 0
-    else:
-        n = int(n.split()[0])
 
 if n > 0:
     infer = []
