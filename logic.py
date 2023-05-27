f4167b060af4b1df685f28aefefb8c9899ba9631
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-27 15:12:45 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -2,24 +2,17 @@ import streamlit as st
 
 st.title("수리논리학 계산기")
 
-buttons = ["∧(and)", "∨(or)", "¬(not)", "→(imply)", "↔(equal)"]
-
-
-
 if st.button("∧(and)", key="and"):
-    st.text_input(label="", value=str("∧"), key="and_in", persist=True)
+    st.text_input(label="∧(and)", value=str("∧"), key="and_in", persist=True)
 
 if st.button("∨(or)", key="or"):
-    st.text_input(label="", value=str("∨"), key="or_in", persist=True)
+    st.text_input(label="∨(or)", value=str("∨"), key="or_in", persist=True)
     
 if st.button("¬(not)", key="not"):
-    st.text_input(label="", value=str("¬"), key="not_in", persist=True)
+    st.text_input(label="¬(not)", value=str("¬"), key="not_in", persist=True)
 
 if st.button("→(imply)", key="imply"):
-    st.text_input(label="", value=str("→"), key="imply_in", persist=True)
+    st.text_input(label="→(imply)", value=str("→"), key="imply_in", persist=True)
 
 if st.button("↔(equal)", key="equal"):
-    st.text_input(label="", value="↔", key="equal_in", persist=True)
-    
-for button in buttons:
-    st.button(button, key=button)
+    st.text_input(label="↔(equal)", value="↔", key="equal_in", persist=True)
