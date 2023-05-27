35aa825e5847852f5ac21e4335aed3607a9d93ae
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-27 15:11:00 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -4,4 +4,22 @@ st.title("수리논리학 계산기")
 
 buttons = ["∧(and)", "∨(or)", "¬(not)", "→(imply)", "↔(equal)"]
 
-st.button_group("Logical Operators", buttons=buttons)
+
+
+if st.button("∧(and)", key="and"):
+    st.text_input(label="", value=str("∧"), key="and_in", persist=True)
+
+if st.button("∨(or)", key="or"):
+    st.text_input(label="", value=str("∨"), key="or_in", persist=True)
+    
+if st.button("¬(not)", key="not"):
+    st.text_input(label="", value=str("¬"), key="not_in", persist=True)
+
+if st.button("→(imply)", key="imply"):
+    st.text_input(label="", value=str("→"), key="imply_in", persist=True)
+
+if st.button("↔(equal)", key="equal"):
+    st.text_input(label="", value="↔", key="equal_in", persist=True)
+    
+for button in buttons:
+    st.button(button, key=button)
