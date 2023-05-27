b227b70ab3202a2fb904a752b77611715cc2c4d1
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-27 15:17:02 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -3,16 +3,16 @@ import streamlit as st
 st.title("수리논리학 계산기")
 
 if st.button("∧(and)", key="and"):
-    st.text_input(label="∧(and)", value=str("∧"), key="and_in", persist=True)
+    st.text_input(label="∧(and)", value="∧", key="and_in", persist=True)
 
 if st.button("∨(or)", key="or"):
-    st.text_input(label="∨(or)", value=str("∨"), key="or_in", persist=True)
+    st.text_input(label="∨(or)", value="∨", key="or_in", persist=True)
     
 if st.button("¬(not)", key="not"):
-    st.text_input(label="¬(not)", value=str("¬"), key="not_in", persist=True)
+    st.text_input(label="¬(not)", value="¬", key="not_in", persist=True)
 
 if st.button("→(imply)", key="imply"):
-    st.text_input(label="→(imply)", value=str("→"), key="imply_in", persist=True)
+    st.text_input(label="→(imply)", value="→", key="imply_in", persist=True)
 
 if st.button("↔(equal)", key="equal"):
     st.text_input(label="↔(equal)", value="↔", key="equal_in", persist=True)
