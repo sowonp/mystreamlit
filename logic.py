32dd3bf62baf9d0a71bd9c96533717f9fb245831
박소원 <126738099+sowonp@users.noreply.github.com>
2023-05-27 15:51:45 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,37 +1,44 @@
+#Scripts\activate.bat
+#streamlit run app2.py
+
 import openai
 import streamlit as st
 
 messages = []
-content = ""
+content1 = ""
+content2 = ""
 chat_response = ""
 infer = [0, 0, 0, 0, 0]
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 st.title("수리논리학 계산기")
 number_string = st.number_input("개별적 사실을 몇 개 입력받겠습니까? (최대 5개)", 1, 5)
 
 number = int(number_string)
-
-st.text("논리식 사용 시, 해당 기호들을 복사하여 사용하세요.")
-st.text("¬ ∧ ∨ →")
+st.markdown("논리식 사용 시, 해당 기호들을 복사하여 사용하세요.")
+st.markdown("¬   ∧    ∨    →")
 
 for i in range(number):
-    infer[i] = st.text_input(f"개별적 사실{i+1}", value = "홈플러스 = (5000 → 라면) ∧ (3000 → 커피)", key = f"infer{i}")
-#레스토랑 = (홈플러스 → (5000 → 라면)) ∧ (홈플러스 → (3000 → 커피)) ∧ ((라면 ∧ 커피) → 라면 세트)
-
+    infer[i] = st.text_input(f"개별적 사실 {i+1}", key = f"infer{i}")
     
-Ginfer2 = st.text_input("질문", value = "손님 = 레스토랑 → (5000 → 라면 세트)", key = "infer5")
+Ginfer2 = st.text_input("질문", key = "infer5")
+
+st.text("<예시>")
+st.text("개별적 사실 1: 홈플러스 = (5000 → 라면) ∧ (3000 → 커피)")
+st.text("개별적 사실 2: 레스토랑 = (홈플러스 → (5000 → 라면)) ∧ (홈플러스 → (3000 → 커피)) ∧ ((라면 ∧ 커피) → 라면 세트)")
+st.text("질문: 손님 = 레스토랑 → (5000 → 라면 세트)")
 if st.button("입력", key = "button"):
     for i in range(number):
-        content += f'"{infer[i]}", '
-    content += f'일 때, "{Ginfer2}"라는 문장은 성립하는가?'
+        content1 += f'"{infer[i]}", '
+    content1 += f'앞의 문장들은 모두 참이다.'
+    content2 = f'"{Ginfer2}"라는 문장을 참 또는 거짓으로 판단해라.'
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        #temperaturn = 9,
         messages=[
-            {"role": "system", "content": "넌 똑똑한 소비자야."},
-            {"role": "user", "content": content}
+            {"role": "system", "content": "넌 논리식 계산기야."},
+            {"role": "user", "content": content1},
+            {"role": "user", "content": content2}
         ]
     )
 
