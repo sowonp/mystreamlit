77732bb74a5e6922dcaeefcba7eb5dd58e9a8b64
박소원 <126738099+sowonp@users.noreply.github.com>
2023-06-19 22:12:05 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,47 +1,79 @@
-#Scripts\activate.bat
-#streamlit run app2.py
-
-import openai
 import streamlit as st
+import three
+import vworld3d
 
-messages = []
-content1 = ""
-content2 = ""
-chat_response = ""
-infer = [0, 0, 0, 0, 0]
+# Set the Vworld3d API key
+vworld3d.setApiKey("your_api_key")
 
-openai.api_key = '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'
+# Load the Vworld3d map
+vworld3d.loadMap("https://apis.vworld.kr/vworld3d/map/1111111111111111")
 
-st.title("수리논리학 계산기")
-number_string = st.number_input("개별적 사실을 몇 개 입력받겠습니까? (최대 5개)", 1, 5)
+# Create a Three.js scene
+scene = three.Scene()
 
-number = int(number_string)
-st.markdown("논리식 사용 시, 해당 기호들을 복사하여 사용하세요.")
-st.markdown("¬   ∧    ∨    →")
+# Add a natural conservation area to the map
+naturalConservationArea = three.Object3D()
+naturalConservationArea.geometry = three.SphereGeometry(1000, 100, 100)
+naturalConservationArea.material = three.MeshBasicMaterial({
+  color: 0x00FF00,
+})
+scene.add(naturalConservationArea)
 
-for i in range(number):
-    infer[i] = st.text_input(f"개별적 사실 {i+1}", key = f"infer{i}")
-    
-Ginfer2 = st.text_input("질문", key = "infer5")
+# Add an airway to the map
+airway = three.Object3D()
+airway.geometry = three.CylinderGeometry(100, 100, 1000)
+airway.material = three.MeshBasicMaterial({
+  color: 0x0000FF,
+})
+scene.add(airway)
 
-st.text("<예시>")
-st.text("개별적 사실 1: 홈플러스 = (5000 → 라면) ∧ (3000 → 커피)")
-st.text("개별적 사실 2: 레스토랑 = (홈플러스 → (5000 → 라면)) ∧ (홈플러스 → (3000 → 커피)) ∧ ((라면 ∧ 커피) → 라면 세트)")
-st.text("질문: 손님 = 레스토랑 → (5000 → 라면 세트)")
-if st.button("입력", key = "button"):
-    for i in range(number):
-        content1 += f'"{infer[i]}", '
-    content1 += f'앞의 문장들은 모두 참이다.'
-    content2 = f'"{Ginfer2}"라는 문장을 참 또는 거짓으로 판단해라.'
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "넌 논리식 계산기야."},
-            {"role": "user", "content": content1},
-            {"role": "user", "content": content2}
-        ]
-    )
+# Create a camera
+camera = three.PerspectiveCamera(75, st.session_state.width / st.session_state.height, 1, 1100)
+camera.position.z = 500
 
-# 응답 결과 표시
-    chat_response = completion.choices[0].message.content
-    st.text(chat_response)
+# Create a renderer
+renderer = three.WebGLRenderer()
+renderer.setSize(st.session_state.width, st.session_state.height)
+st.write(renderer)
+
+# Create a scene controller
+sceneController = three.OrbitControls(camera, renderer.domElement)
+
+# Start the animation loop
+requestAnimationFrame(render)
+
+def render(t):
+  # Update the camera
+  sceneController.update(t)
+
+  # Render the scene
+  renderer.render(scene, camera)
+
+  # Request the next animation frame
+  requestAnimationFrame(render)
+
+st.title("Vworld 3D Map")
+
+st.write("This is a 3D map of Vworld. You can add natural conservation areas, airways, and other data to the map using Streamlit and Vworld APIs.")
+
+st.write("To add a natural conservation area, click the 'Add Natural Conservation Area' button.")
+
+st.write("To add an airway, click the 'Add Airway' button.")
+
+st.write("To add other data to the map, use the Streamlit and Vworld APIs.")
+
+st.write("To interact with the map, use the mouse and keyboard.")
+
+st.write("To zoom in and out, use the scroll wheel.")
+
+st.write("To pan the map, hold down the left mouse button and drag.")
+
+st.write("To rotate the map, hold down the right mouse button and drag.")
+
+st.write("To change the camera view, use the controls in the bottom right corner of the map.")
+
+st.write("To learn more about Vworld, visit the Vworld website.")
+
+st.write("To learn more about Streamlit, visit the Streamlit website.")
+
+st.write("To get started with Vworld and Streamlit, visit the Vworld and Streamlit documentation.")
